e96ccfb30f5002cdfa4103ff7776c3c63a64f7f1
vmorenoluna <morenolunav@gmail.com>
2020-02-03 22:56:24 +0000

    Added Chords functions for exercise 2.1

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+import org.jfugue.player.Player
+
+object Main extends App {
+
+  val player: Player = new Player
+  player.play("C D E F G A B")
+
+}
--- src/main/scala/chapter2/Chords.scala
+++ src/main/scala/chapter2/Chords.scala
@@ -1,6 +1,6 @@
 package chapter2
 
-import chapter2.Types.Pitch
+import chapter2.Types.{Duration, Octave, Pitch}
 import Music._
 
 object Chords {
@@ -8,4 +8,24 @@ object Chords {
   def dMinor(): Music[Pitch] = d(4,wn) :=: f(4,wn) :=: a(4,wn)
   def gMajor(): Music[Pitch] = g(4,wn) :=: b(4,wn) :=: d(4,wn)
   def cMajor(): Music[Pitch] = c(4,wn) :=: e(4,wn) :=: g(4,wn)
+
+  def minor(root: Music[Pitch]): Music[Pitch] = {
+    val minorThird = transpose(3, root)
+    val perfectFifth = transpose(7, root)
+    root :=: minorThird :=: perfectFifth
+  }
+
+  def major(root: Music[Pitch]): Music[Pitch] = {
+    val majorThird = transpose(4, root)
+    val perfectFifth = transpose(7, root)
+    root :=: majorThird :=: perfectFifth
+  }
+
+  def twoFiveOne(p: Pitch, d: Duration): Music[Pitch] = {
+    val ii = minor(transpose(3, note(d, p)))
+    val V = major(transpose(7, note(d, p)))
+    val I = major(transpose(12, note(2*d, p)))
+    ii :+: V :+: I
+  }
+
 }

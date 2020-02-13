beb61a93c8aa04e4d1131e2f2a18f8567028f519
vmorenoluna <morenolunav@gmail.com>
2020-02-13 21:26:14 +0000

    Refined test in exercise-2-1

--- the-haskell-school-of-music/exercise-2-1.hs
+++ the-haskell-school-of-music/exercise-2-1.hs
@@ -1,7 +1,12 @@
 import Euterpea
 
-main = play $ twoFiveOne (C,4) wn
+main = play $ t251 :+: wnr :+: twoFiveOne (C,4) wn
 
+t251 :: Music Pitch
+t251 = let dMinor = d 4 wn :=: f 4 wn :=: a 4 wn
+           gMajor = g 4 wn :=: b 4 wn :=: d 5 wn
+           cMajor = c 4 bn :=: e 4 bn :=: g 4 bn
+       in dMinor :+: gMajor :+: cMajor
 
 minor :: Music Pitch -> Music Pitch 
 minor root = root :=: transpose 3 root :=: transpose 7 root
@@ -10,8 +15,8 @@ major :: Music Pitch -> Music Pitch
 major root = root :=: transpose 4 root :=: transpose 7 root
     
 twoFiveOne :: Pitch -> Dur -> Music Pitch 
-twoFiveOne p d = let ii = minor(transpose 3 (note d p))
+twoFiveOne p d = let ii = minor(transpose 2 (note d p))
                      v = major(transpose 7 (note d p))
-                     i = major(transpose 12 (tempo 0.5 (note d p)))
+                     i = major(tempo 0.5 (note d p))
                  in ii :+: v :+: i
  

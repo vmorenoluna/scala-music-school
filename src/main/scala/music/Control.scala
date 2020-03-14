223d7d7cc4b34378adfec5f11596cb35cbff6a8d
vmorenoluna <morenolunav@gmail.com>
2020-03-14 10:54:58 +0000

    Renamed Modes to Mode

--- src/main/scala/music/Control.scala
+++ src/main/scala/music/Control.scala
@@ -8,5 +8,5 @@ final case class Tempo(value: TempoValue) extends Control
 final case class Transpose(value: AbsPitch) extends Control
 final case class Instrument(instrumentName: InstrumentName) extends Control
 final case class Phrase(attributes: List[PhraseAttribute]) extends Control
-final case class KeySig(pitchClass: PitchClass, mode: Modes) extends Control
+final case class KeySig(pitchClass: PitchClass, mode: Mode) extends Control
 final case class Custom(value: String) extends Control
---
+++ src/main/scala/music/Mode.scala
@@ -0,0 +1,14 @@
+package music
+
+sealed trait Mode
+
+final case object Major extends Mode
+final case object Minor extends Mode
+final case object Ionian extends Mode
+final case object Dorian extends Mode
+final case object Phrygian extends Mode
+final case object Lydian extends Mode
+final case object Mixolydian extends Mode
+final case object Aeolian extends Mode
+final case object Locrian extends Mode
+final case class CustomMode(name: String) extends Mode

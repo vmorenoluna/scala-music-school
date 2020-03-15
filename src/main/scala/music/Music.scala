95db9e7abd884971a71df4a71a25033195750bc3
vmorenoluna <morenolunav@gmail.com>
2020-03-15 12:33:41 +0000

    Formatting

--- src/main/scala/music/Music.scala
+++ src/main/scala/music/Music.scala
@@ -94,11 +94,8 @@ final object Music {
   def denr(): Music[Pitch] = rest(den)
   def dsnr(): Music[Pitch] = rest(dsn)
   def dtnr(): Music[Pitch] = rest(dtn)
-
   def ddhnr(): Music[Pitch] = rest(ddhn)
-
   def ddqnr(): Music[Pitch] = rest(ddqn)
-
   def ddenr(): Music[Pitch] = rest(dden)
 
   def line[A](notes: List[Music[A]]): Music[A] = notes.foldRight(rest[A](0))(_ :+: _)

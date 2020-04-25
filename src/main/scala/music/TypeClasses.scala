d13ad455e82a558c2f29beb26361feaa973877c0
vmorenoluna <morenolunav@gmail.com>
2020-04-25 12:22:21 +0100

    Refactored PrimitiveTypeClassInstances

--- src/main/scala/music/TypeClasses.scala
+++ src/main/scala/music/TypeClasses.scala
@@ -17,46 +17,23 @@ object PrimitiveTypeClassInstances {
 
   implicit val primitiveInstance: PrimitiveTypeClass[Primitive[Pitch]] = new PrimitiveTypeClass[Primitive[Pitch]] {
     override def eqv(x: Primitive[Pitch], y: Primitive[Pitch]): Boolean = (x, y) match {
-      case (a@Note(_, _), b@Note(_, _)) => notePrimitiveInstance.eqv(a, b)
-      case (a@Rest(_), b@Rest(_)) => restPrimitiveInstance.eqv(a, b)
+      case (Note(d1, p1), Note(d2, p2)) => d1 === d2 && p1 === p2
+      case (Rest(d1), Rest(d2)) => d1 === d2
       case _ => false
     }
 
     override def neqv(x: Primitive[Pitch], y: Primitive[Pitch]): Boolean = (x, y) match {
-      case (a@Note(_, _), b@Note(_, _)) => notePrimitiveInstance.neqv(a, b)
-      case (a@Rest(_), b@Rest(_)) => restPrimitiveInstance.neqv(a, b)
+      case (Note(d1, p1), Note(d2, p2)) => (d1 =!= d2) || (p1 =!= p2)
+      case (Rest(d1), Rest(d2)) => d1 =!= d2
       case _ => false
     }
 
     override def compare(x: Primitive[Pitch], y: Primitive[Pitch]): Int = (x, y) match {
-      case (a@Note(_, _), b@Note(_, _)) => notePrimitiveInstance.compare(a, b)
-      case (a@Rest(_), b@Rest(_)) => restPrimitiveInstance.compare(a, b)
-    }
-  }
-
-  implicit val notePrimitiveInstance: PrimitiveTypeClass[Note[Pitch]] = new PrimitiveTypeClass[Note[Pitch]] {
-    override def eqv(x: Note[Pitch], y: Note[Pitch]): Boolean =
-      x.duration === y.duration && x.features === y.features
-
-    override def neqv(x: Note[Pitch], y: Note[Pitch]): Boolean =
-      x.duration =!= y.duration || x.features =!= y.features
-
-    override def compare(x: Note[Pitch], y: Note[Pitch]): Int = (x, y) match {
       case (Note(d1, p1), Note(d2, p2)) if (p1 gt p2) || (p1 === p2 && d1 > d2) => 1
       case (Note(d1, p1), Note(d2, p2)) if p1 === p2 && d1 === d2 => 0
       case (Note(d1, p1), Note(d2, p2)) if (p1 lt p2) || (p1 === p2 && d1 < d2) => -1
+      case (Rest(d1), Rest(d2)) => d1 compare d2
     }
-  }
-
-  implicit val restPrimitiveInstance: PrimitiveTypeClass[Rest[Pitch]] = new PrimitiveTypeClass[Rest[Pitch]] {
-    override def eqv(x: Rest[Pitch], y: Rest[Pitch]): Boolean =
-      x.duration === y.duration
-
-    override def neqv(x: Rest[Pitch], y: Rest[Pitch]): Boolean =
-      x.duration =!= y.duration
-
-    override def compare(x: Rest[Pitch], y: Rest[Pitch]): Int =
-      x.duration compare y.duration
   }
 
 }
@@ -70,10 +47,13 @@ sealed trait MusicTypeClass[A] {
 }
 
 object PrimitiveOps {
+
   final implicit class PrimitiveEqOps[A](private val self: A) {
     def ===(other: A)(implicit p: PrimitiveTypeClass[A]): Boolean = p.eqv(self, other)
+
     def =!=(other: A)(implicit p: PrimitiveTypeClass[A]): Boolean = p.neqv(self, other)
   }
+
 }
 
 object MusicTypeClassInstances {

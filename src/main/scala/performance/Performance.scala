84cb69535442815ae5cd6975330d80392c11f4fb
vmorenoluna <morenolunav@gmail.com>
2020-05-06 23:06:06 +0100

    Refactored Player and Performance to solve generic types issues

--- src/main/scala/performance/Performance.scala
+++ src/main/scala/performance/Performance.scala
@@ -1,9 +1,10 @@
 package performance
 
-import music.{:+:, :=:, Custom, Instrument, KeySig, Modify, Music, Note, Phrase, PhraseAttribute, Prim, Rest, Tempo, Transpose}
+import music.Music1.Note1
+import music.{:+:, :=:, Custom, Instrument, KeySig, Modify, Music, Note, Phrase, Prim, Rest, Tempo, Transpose}
 import music.Types.Duration
 import performance.Performance.DurT
-import performance.players.Player
+import performance.players.Player.players
 import spire.math.Rational
 
 object Performance {
@@ -11,37 +12,33 @@ object Performance {
   type Performance = List[MEvent]
   type PTime = Rational
   type DurT = Rational
-  type PlayerName = String
-  type PMap[A] = PlayerName => Player[A]
-  type NoteFun[A] = Context[A] => Duration => A => Performance
-  type PhraseFun[A] = PMap[A] => Context[A] => List[PhraseAttribute] => Music[A] => (Performance, DurT)
 
   def perform(): Performance = ??? // TODO default perform
 
-  def hsomPerform[A](pm: PMap[A], c: Context[A], m: Music[A]): Performance =
-    perf(pm, c, m)._1
+  def hsomPerform(c: Context[Note1], m: Music[Note1]): Performance =
+    perf(c, m)._1
 
-  def perf[A](pm: PMap[A], c: Context[A], m: Music[A]): (Performance, DurT) = m match {
+  def perf[A](c: Context[Note1], m: Music[Note1]): (Performance, DurT) = m match {
     case Prim(Note(d, p)) => (c.cPlayer.playNote(c)(d)(p), d * c.cDur)
     case Prim(Rest(d)) => (List.empty, d * c.cDur)
     case :+:(m1, m2) => {
-      val (pf1, d1) = perf(pm, c, m1)
-      val (pf2, d2) = perf(pm, c.copy(cTime = c.cTime + d1), m2)
+      val (pf1, d1) = perf(c, m1)
+      val (pf2, d2) = perf(c.copy(cTime = c.cTime + d1), m2)
       (pf1 ++ pf2, d1 + d2)
     }
     case :=:(m1, m2) => {
-      val (pf1, d1) = perf(pm, c, m1)
-      val (pf2, d2) = perf(pm, c, m2)
+      val (pf1, d1) = perf(c, m1)
+      val (pf2, d2) = perf(c, m2)
       (merge(pf1, pf2), d1 max d2)
     }
-    case Modify(Tempo(r), m) => perf(pm, c.copy(cDur = c.cDur / r), m)
-    case Modify(Transpose(p), m) => perf(pm, c.copy(cPch = c.cPch + p), m)
-    case Modify(Instrument(i), m) => perf(pm, c.copy(cInst = i), m)
-    case Modify(KeySig(pc, mo), m) => perf(pm, c.copy(cKey = (pc, mo)), m)
-    case Modify(Phrase(pas), m) => c.cPlayer.interpPhrase(pm)(c)(pas)(m)
+    case Modify(Tempo(r), m) => perf(c.copy(cDur = c.cDur / r), m)
+    case Modify(Transpose(p), m) => perf(c.copy(cPch = c.cPch + p), m)
+    case Modify(Instrument(i), m) => perf(c.copy(cInst = i), m)
+    case Modify(KeySig(pc, mo), m) => perf(c.copy(cKey = (pc, mo)), m)
+    case Modify(Phrase(pas), m) => c.cPlayer.interpPhrase(c, pas, m)
     case Modify(Custom(s), m) =>
-      if (s.take(7) == "Player ") perf(pm, c.copy(cPlayer = pm(s.drop(7))), m)
-      else perf(pm, c, m)
+      if (s.take(7) == "Player ") perf(c.copy(cPlayer = players.get(s.drop(7)).get), m)
+      else perf(c, m)
   }
 
   def merge(p1: Performance, p2: Performance): Performance = (p1, p2) match {
--- src/main/scala/performance/players/Player.scala
+++ src/main/scala/performance/players/Player.scala
@@ -1,46 +1,58 @@
 package performance.players
 
-import music.{NoteAttribute, Params, Volume}
-import music.Types.{Duration, Pitch, absPitch}
+import music.Music1.Note1
+import music.{Accent, Art, Dyn, Legato, Music, NoteAttribute, Params, PhraseAttribute, Staccato, Volume}
+import music.Types.{Duration, absPitch}
 import performance.{Context, MEvent}
-import performance.Performance.{NoteFun, PhraseFun, PlayerName}
+import performance.Performance.{DurT, Performance, perf}
 
 trait Player[A] {
-  def pName: PlayerName
-
-  def playNote: NoteFun[A]
-
-  def interpPhrase: PhraseFun[A]
-
+  def pName: String
+  def playNote(c: Context[A])(d: Duration)(a: A): Performance
+  def interpPhrase(c: Context[A], pas: List[PhraseAttribute], m: Music[A]): (Performance, DurT)
   def getName(): String = s"Player $pName"
 }
 
-case class DefPlayer[A]() extends Player[A] {
+object Player {
+  val players = Map(
+    "DefPlayer" -> DefPlayer
+  ).withDefaultValue(DefPlayer)
+}
 
-  override def pName: PlayerName = "DefPlayer"
+object DefPlayer extends Player[Note1] {
 
-  override def playNote: NoteFun[A] = ???
+  override def pName: String = "DefPlayer"
 
-  private val defPlayNote = (nasHandler: Context[(Pitch, List[A])] => (A, MEvent) => MEvent) =>
-    (c: Context[(Pitch, List[A])]) => (d: Duration) => (p: Pitch, nas: List[A]) => {
-      val initEv = MEvent(
-        eTime = c.cTime,
-        eInst = c.cInst,
-        ePitch = absPitch(p) + c.cPch,
-        eDur = d * c.cDur,
-        eVol = c.cVol,
-        eParams = List.empty
-      )
-      nas.foldRight(initEv)(nasHandler(c))
+  override def playNote(c: Context[Note1])(d: Duration)(n: Note1): Performance = {
+    val initEv = MEvent(
+      eTime = c.cTime,
+      eInst = c.cInst,
+      ePitch = absPitch(n._1) + c.cPch,
+      eDur = d * c.cDur,
+      eVol = c.cVol,
+      eParams = List.empty
+    )
+    List(n._2.foldRight(initEv)(nasHandler(c)))
   }
 
-  private val defNasHandler = (c: Context[A]) => (na: NoteAttribute, ev: MEvent) =>
+  private def nasHandler(c: Context[Note1])(na: NoteAttribute, ev: MEvent): MEvent =
     (c, na, ev) match {
       case (_, Volume(v), ev) => ev.copy(eVol = v)
       case (_, Params(pms), ev) => ev.copy(eParams = pms)
       case (_, _, ev) => ev
     }
 
-  override def interpPhrase: PhraseFun[A] = ???
+  override def interpPhrase(c: Context[Note1], pas: List[PhraseAttribute], m: Music[Note1]): (Performance, DurT) = {
+    val (pf, dur) = perf(c, m)
+    (pas.foldRight(pf)(pasHandler), dur)
+  }
+
+  private def pasHandler(pa: PhraseAttribute, p: Performance): Performance =
+    pa match {
+      case Dyn(Accent(x)) => p.map(e => e.copy(eVol = x.intValue * p.head.eVol))
+      case Art(Staccato(x)) => p.map(e => e.copy(eDur = x.intValue * p.head.eDur))
+      case Art(Legato(x)) => p.map(e => e.copy(eDur = x.intValue * p.head.eDur))
+      case _ => p
+    }
 
 }
